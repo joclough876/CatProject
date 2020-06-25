af7a51beb5086d0fed1017e723286a7009e5d703
jodia <jodia@DESKTOP-0TR476P>
2020-06-25 18:10:27 -0500

    Created BritishShortHairTest and BritishShortHair

---
+++ CatProject/src/com/clough/cat_project/BritishShortHair.java
@@ -0,0 +1,10 @@
+package com.clough.cat_project;
+
+public class BritishShortHair extends Cat {
+
+	public BritishShortHair(String name, String gender, int age, double weight, boolean isAMouser) {
+		// TODO Auto-generated constructor stub
+		super(name, gender, age, weight, isAMouser);
+	}
+
+}
--- CatProject/test/com/clough/cat_project/BritishShortHairTest.java
+++ CatProject/test/com/clough/cat_project/BritishShortHairTest.java
@@ -5,24 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class BritishShortHairTest extends CatTest{
-	String gender;
-	int age;
-	double weight;
-	Boolean isAMouser;
-	String name;
-
-	private static Cat garfield;
+ class BritishShortHairTest {
+	
+	 //public BritishShortHairTest(String name, String gender, int age, double weight, boolean isAMouser) {
+		 //super(name, gender, age, weight, isAMouser);
+	 
+	 //}
+	private static BritishShortHair garfield;
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		//super(name, gender, age, weight, isAMouser);
-		//thisname.name;
-		garfield = new Cat("Garfield", "male", 10, 20.75, false);
+
+		
+		garfield = new BritishShortHair("Garfield", "male", 10, 20.75, false);
 	}
 
 	@Test
 	void cat_GivenFiveParameters_ShouldConstructCat(){
-		assertTrue(garfield instanceof Cat);
+		assertTrue(garfield instanceof BritishShortHair);
 	}
 
 }
